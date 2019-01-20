349304787836484393d88df3f21554ec3b43f128
Chenghao Xiong <chenghaoxiong@chenghaodemacbook-pro.local>
2019-01-20 12:40:26 -0700

    Initial Commit

---
+++ iOS_Calculatlor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Calculatlor
+//
+//  Created by Chenghao Xiong on 1/20/19.
+//  Copyright © 2019 Chenghao Xiong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ iOS_CalculatlorTests/iOS_CalculatlorTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_CalculatlorTests.swift
+//  iOS_CalculatlorTests
+//
+//  Created by Chenghao Xiong on 1/20/19.
+//  Copyright © 2019 Chenghao Xiong. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Calculatlor
+
+class iOS_CalculatlorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
